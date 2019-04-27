5f37db9477eca093cceb4275e5f66051b35c699d
Laura Kocubinski <laura@Lauras-MacBook-Pro.local>
2019-04-27 16:27:09 -0400

    second commit

--- README.md
+++ README.md
@@ -4,17 +4,17 @@ Smart reply bot implemented in Python.
 
 ## What Does It Do?
 
-The objective of this project is to create a bot that suggests short responses to a text or email message. A reply bot is a type of chatbot. The goal of a chatbot is to mimic written or spoken speech as to simulate a conversation with a human.
+The objective of this project is to create a bot that suggests short responses to a text or email message using Natural Language Processing. A reply bot is a type of chatbot. The goal of a chatbot is to mimic written or spoken speech as to simulate a conversation with a human.
 
-This bot is designed to reply to casual conversation. This will require both analyzing and generating text which will be done using Natural Language Processing (NLP).
+The scope of bot is casual conversation.
 
 From a high-level, there are two variants of chatbots, **rule-based** and **self-learning**:
-* A rule-based bot responds via pattern matching and rules.
-* A self-learning bot responds via using machine learning.
-    * A retrieval-based chatbot replies by locating the best response from a database.
-    * A generative-based chatbot use deep learning methods to respond and can generate a response it has never seen before.
+* A **rule-based** bot responds via pattern matching and rules.
+* A **self-learning** bot responds via using machine learning.
+    * A **retrieval-based** chatbot replies by locating the best response from a database (corpus).
+    * A **generative-based** chatbot use deep learning methods to respond and can generate a response it has never seen before.
 
-In this project, I have implemented a rule-based, retrieval-based and machine learning-based reply bot.
+In order to understand the tradeoffs between the different types of chatbots, I implemented a rule-based, a retrieval-based and a machine learning-based reply bot.
 
 ## Built With
 
@@ -22,29 +22,82 @@ In this project, I have implemented a rule-based, retrieval-based and machine le
 * [NLTK](https://www.nltk.org/)
 * TensorFlow and TFLearn
 
+## Design
+
+### Rule-based bot
+
+A rule-based chatbot is the simplest type of bot. This type of bot searches for predefined patterns in the input and uses a set of rules (if-else logic) to suggest replies. The suggested replies, in this case, are predefined. We can implement pattern matching on an input message by using regular expressions and applying rules with if-else statements.
+
+For example, to search for a greeting we can use this as our regex pattern:
+
+```python
+greeting_str = 'hi|hello|sup|hey|good\s+[morning|afternoon|evening]
+```
+
+If the pattern is found, we suggest predefined greeting responses:
+
+```python
+greeting_response = ["hi", "hey", "hello"]
+```
+
+Altogether we have
+
+```python
+if re.search(greeting_str, user_input):
+    return greeting_response
+```
+
+For the reply bot, I have defined 8 simple rules:
+* Greeting
+* Goodbye
+* How are you
+* Thank you
+* Do you/will you/can you/would you
+* Are you
+* When
+* What’s up
+
+Each rule has an associated reply:
+
+```python
+greeting_response = ["hi", "hello", "hey"]
+goodbye_response = ["bye", "talk to you later"]
+thank_response = ['happy to help','don\'t mention it','my pleasure']
+inquiry_response = ['i\'m doing ok','ok','i\'ve been better']
+future_response = ['yes','no','maybe']
+what_you_response = ['nothing', 'not much']
+are_you_response = ['yes','no', 'maybe']
+when_response = ['soon','not now']
+no_response = ['[No Suggestion]']
+```
+
 ## Usage
 
 Execute the program
 
-```python
+```
 python rule_based_reply.py
+```
+
+The program will then request a message
+
+```
 * Hello! Type in a message and I will suggest some replies! If you'd like to exit please type quit!
 >>>
 ```
 
-Try different phrases and the reply bot will generate suggested replies
+Try different different messages and the reply bot will generate suggested replies or [No Suggestion]
 
-```python
->>> hello
+```
 >>> hello
 * hi
 * hey
 * hello
 ```
 
-To quit, simple type quit
+To quit, type quit
 
-```python
+```
 >>> quit
 ```
 
--- src/ml_reply.py
+++ src/ml_reply.py
@@ -1,4 +1,3 @@
-
 import nltk
 import numpy as np
 import tflearn
@@ -12,8 +11,6 @@ stemmer = LancasterStemmer()
 
 from classes_dict import *
 
-# sent_tokens = {}
-
 my_words = []
 my_classes = []
 my_doc = []
@@ -21,24 +18,24 @@ ignore_words = ['?']
 
 # pre-process text from classes_dict
 for some_class in classes_dict:
-
+    
     my_classes.append(some_class)
-
+    
     for some_pattern in classes_dict[some_class]["pattern"]:
-
+        
         temp_words = []
-
+        
         raw_words = some_pattern
         # raw_words = ' '.join(raw_words)
         word_tokens = nltk.word_tokenize(raw_words)
-
+        
         for some_word in word_tokens:
             if some_word not in ignore_words:
                 stemmed_word = stemmer.stem(some_word.lower())
                 my_words.append(stemmed_word)
                 temp_words.append(stemmed_word)
 
-        my_doc.append((temp_words, some_class))
+    my_doc.append((temp_words, some_class))
 
 my_words = sorted( list(set(my_words)) ) # remove duplicate words
 my_classes = sorted( list(set(my_classes)) )
@@ -48,10 +45,10 @@ output = []
 output_empty = [0] * len(my_classes)
 
 for some_doc in my_doc:
-
+    
     bag = []
     pattern_words = some_doc[0]
-
+    
     # create bag of words array
     for some_word in my_words:
         if some_word in pattern_words:
@@ -59,24 +56,23 @@ for some_doc in my_doc:
         else:
             bag.append(0)
 
-    # output is a '0' for each tag and '1' for current tag
-    output_row = list(output_empty)
-    output_row[ my_classes.index(some_doc[1]) ] = 1
+output_row = list(output_empty)
+output_row[my_classes.index(some_doc[1])] = 1
 
-    training.append([bag, output_row])
+training.append([bag, output_row])
 
-# shuffle our features and turn into np.array
+# shuffle training data and put into array
 random.shuffle(training)
 training = np.array(training)
 
-# create train and test lists
+# create train lists
 train_x = list(training[:,0])
 train_y = list(training[:,1])
 
 # reset underlying graph data
 tf.reset_default_graph()
 
-# build neural network
+# build nn model
 net = tflearn.input_data(shape = [None, len(train_x[0])])
 net = tflearn.fully_connected(net, 8)
 net = tflearn.fully_connected(net, 8)
@@ -86,11 +82,12 @@ net = tflearn.regression(net)
 # define model and setup tensorboard
 model = tflearn.DNN(net, tensorboard_dir = 'tflearn_logs')
 
-# start training
+# train model
 # model.fit(train_x, train_y, n_epoch = 500, batch_size = 8, show_metric = True)
 # model.save('model_stupid.tflearn')
 # pickle.dump( {'my_words':my_words, 'my_classes':my_classes, 'train_x':train_x, 'train_y':train_y}, open( "training_data", "wb" ) )
 
+# load model
 model.load('./model_laura.tflearn')
 
 # import pickle
@@ -100,37 +97,50 @@ model.load('./model_laura.tflearn')
 # train_x = data['train_x']
 # train_y = data['train_y']
 
-def clean_up_sentence(sentence):
-
+def tokenize_input(sentence):
+    
     sentence_words = nltk.word_tokenize(sentence) # tokenize pattern
-    sentence_words = [stemmer.stem(word.lower()) for word in sentence_words] # stem each word
-
+    # sentence_words = [stemmer.stem(word.lower()) for word in sentence_words] # stem each word
+    
+    for some_word in sentence_words:
+        sentence_words[sentence_words.index(some_word)] = stemmer.stem(some_word.lower())
+    
     return sentence_words
 
-def bow(sentence, words):
-
-    sentence_words = clean_up_sentence(sentence)
-    bag = [0] * len(my_words) # bag of words
-
-    for s in sentence_words:
-        for i,w in enumerate(my_words):
-            if w == s:
+def bag(user_input):
+    
+    input_words = tokenize_input(user_input)
+    
+    bag = [0] * len(my_words)
+    
+    for input_word in input_words:
+        for i in range(0,len(my_words)):
+            bag_word = my_words[i]
+            if input_word == bag_word:
                 bag[i] = 1
 
-    return(np.array(bag))
+return(np.array(bag))
 
 error_threshold = 0.95
 
-def classify(sentence):
-    results = model.predict([bow(sentence, my_words)])[0] # generate probabilities from the model
-    results = [[i,r] for i,r in enumerate(results) if r > error_threshold] # filter out predictions below a threshold
-    results.sort(key=lambda x: x[1], reverse=True) # sort by strength of probability
+def classify(user_input):
+    
+    results = model.predict([bag(user_input)])[0] # generate probabilities from the model
+    
+    filtered_results = []
+    
+    for i in range(0, len(results)):
+        this_result = results[i]
+        if this_result > error_threshold:
+            filtered_results.append([i, this_result])
 
-    return_list = []
+filtered_results.sort(key = lambda x: x[1], reverse = True)
 
-    for r in results:
-        return_list.append((my_classes[r[0]], r[1]))
+return_list = []
 
+for i in range(0, len(filtered_results)):
+    return_list.append((my_classes[filtered_results[i][0]], filtered_results[i][1]))
+    
     return return_list # return tuple of intent and probability
 
 print("* Hello! Type in a message and I will suggest some replies! If you'd like to exit please type quit!")
@@ -138,90 +148,31 @@ print("* Hello! Type in a message and I will suggest some replies! If you'd like
 flag = True
 
 while flag:
-
+    
     user_input = raw_input('>>> ').lower() # get input and convert to lowercase
-
+    
     if not re.search('quit', user_input):
-
+        
         some_array = []
-
+        
         some_array = classify(user_input)
-
+        
         # print "predicted class: " + my_classes[pred_class]
         # print "probability: " + str(pred_prob)
-
+        
         if len(some_array) != 0:
-
+            
             # print "predicted class: " + str(some_array[0][0])
             # print "probability: " + str(some_array[0][1])
-
+            
             for response in classes_dict[some_array[0][0]]["response"]:
                 print "* " + response
-
+    
         else:
+            
+            print "[No Suggestion]"
 
-                print "[No Suggestion]"
+else:
+    
+    flag = False
 
-    else:
-
-        flag = False
-
-#
-# def clean_up_sentence(sentence):
-#
-#     sentence_words = nltk.word_tokenize(sentence)
-#
-#     for word in sentence_words:
-#         sentence_words = stemmer.stem( word.lower() )
-#
-#     return sentence_words
-#
-# def bow(sentence, my_words):
-#
-#     # tokenize the pattern
-#     sentence_words = clean_up_sentence(sentence)
-#
-#     # bag of words
-#     # bag = [0] * len(my_words)
-#     bag = []
-#
-#     for some_word in my_words:
-#         if some_word in sentence_words:
-#             bag.append(1)
-#             # print "found " + some_word + " in bag"
-#         else:
-#             bag.append(0)
-#
-#     return bag
-#
-# error_threshold = 0.25
-# def classify(sentence):
-#
-#     bag = bow(sentence, my_words)
-#
-#     pred_prob = model.predict([bag])[0]
-#     index = 0
-#     temp_pred = 0
-#
-#     for some_pred in pred_prob:
-#         if some_pred > temp_pred:
-#             temp_pred = some_pred
-#             best_pred_index = index
-#             best_pred_prob = some_pred
-#         index = index + 1
-#
-#     return best_pred_index, best_pred_prob
-
-
-
-# my_sentence = 'whats up'
-
-# pred_class, pred_prob = classify(my_sentence)
-
-# print "sentence: " + my_sentence
-# print "predicted class: " + my_classes[pred_class]
-# print "probability: " + str(pred_prob)
-
-# print "suggested replies: "
-# for response in classes_dict[my_classes[pred_class]]["response"]:
-#    print "* " + response
